064f6782341fde5e8529d34439eb327fb5902305
Sagar Badami <eparis@atlassian.com>
2020-06-03 17:26:33 +0530

    Initial commit

--- demo/demo.js
+++ demo/demo.js
@@ -5,7 +5,7 @@
  $('#toggle-switcher').click(function(){
 		if($(this).hasClass('open')){
 			$(this).removeClass('open');
-			$('#switch-style').animate({'left':'-220px'});
+			$('#switch-style').animate({'left':'0px'});
 		}else{
 			$(this).addClass('open');
 			$('#switch-style').animate({'left':'0'});
